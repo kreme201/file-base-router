62019d5965ec95b59abbec0407c634ea60ce4b33
kreme201 <kreme201@gmail.com>
2024-02-28 11:25:26 +0900

    데모 어플리케이션 수정

--- demo/public/index.php
+++ demo/public/index.php
@@ -2,13 +2,14 @@
 
 use Kreme\FileBaseRouter\Dispatcher;
 
-require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
+define('APP_PATH', dirname(__DIR__));
+define('COMPONENT_PATH', APP_PATH . '/components');
+
+require_once dirname(APP_PATH) . '/vendor/autoload.php';
 
 $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $dispatcher = new Dispatcher(dirname(__DIR__) . '/pages');
 $template = $dispatcher->dispatch($request_uri);
-
-define('COMPONENT_PATH', dirname(__DIR__) . '/components');
 
 if (false !== $template && file_exists($template)) {
     include $template;
